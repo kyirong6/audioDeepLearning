0a32b35a419180c00d7fae8e371c77d22f426ac3
ckyirong <ckyirong@sfu.ca>
2021-03-16 13:57:45 -0700

    most basic neuron implementation

---
+++ 3-neurons/artificial_neuron.py
@@ -0,0 +1,21 @@
+import math
+
+def sigmoid(x):
+    y = 1.0 / (1 + math.exp(-x))
+    return y
+
+def activate(inputs, weights):
+    # perform net inputs
+    h = 0
+    for x, w in zip(inputs, weights): # unpack two lists at once
+        h += x*w
+
+    # perform activation
+    return sigmoid(h)
+
+
+if __name__ == "__main__":
+    inputs = [0.5, 0.3, 0.2]
+    weights = [0.4, 0.7, 0.2]
+    output = activate(inputs, weights)
+    print(output)

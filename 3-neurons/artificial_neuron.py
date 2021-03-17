fdf8f33cdb6e0269d86c77059a6a45f4cc38f463
ckyirong <ckyirong@sfu.ca>
2021-03-16 21:11:20 -0700

    re formated

--- 3-neurons/artificial_neuron.py
+++ 3-neurons/artificial_neuron.py
@@ -1,14 +1,16 @@
 import math
 
+
 def sigmoid(x):
     y = 1.0 / (1 + math.exp(-x))
     return y
 
+
 def activate(inputs, weights):
     # perform net inputs
     h = 0
-    for x, w in zip(inputs, weights): # unpack two lists at once
-        h += x*w
+    for x, w in zip(inputs, weights):  # unpack two lists at once
+        h += x * w
 
     # perform activation
     return sigmoid(h)

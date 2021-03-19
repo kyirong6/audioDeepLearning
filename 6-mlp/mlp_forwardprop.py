ff7b62a4df8b6719fed9abd1fc5dec29db9070fd
ckyirong <ckyirong@sfu.ca>
2021-03-18 18:16:13 -0700

    implement back propagation

--- 6-mlp/mlp_forwardprop.py
+++ 6-mlp/mlp_forwardprop.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+# save activations and derivatives
+# implement backpropagation
+# implement gradient descent
+# implement train method
+# train our network with dummy dataset
+# make some predictions
+
 
 class MLP:
 
@@ -17,17 +24,66 @@ class MLP:
             w = np.random.rand(layers[i], layers[i + 1])
             self.weights.append(w)
 
-    def forward_propagate(self, inputs):
-        activations = inputs
+        # each array in activations are the activations for a given layer
+        activations = []
+        for i in range(len(layers)):
+            a = np.zeros(layers[i])
+            activations.append(a)
+        self.activations = activations
 
-        for w in self.weights:
+        # each array in derivatives are the derivatives of weights
+        # -1 because for a NN we only have layers - 1 weight matrices
+        derivatives = []
+        for i in range(len(layers) - 1):
+            d = np.zeros((layers[i], layers[i+1]))
+            derivatives.append(d)
+        self.derivatives = derivatives
+
+    def forward_propagate(self, inputs):
+        """Computes forward propagation of the network based on input signals.
+
+        Args:
+            inputs (ndarray): Input signals
+        Returns:
+            activations (ndarray): Output values
+        """
+        activations = inputs
+        self.activations[0] = inputs
+
+        for i, w in enumerate(self.weights):
             # calculate net inputs for a given layer
             net_inputs = np.dot(activations, w)
 
             # calculate the activations
             activations = self._sigmoid(net_inputs)
+            # store activations, i+1 because the 0th index of activations are the inputs.
+            self.activations[i+1] = activations
 
         return activations
+
+    def back_propagate(self, error, verbose=False):
+        # reversed because we need to go backwards
+        # dE/dW_i = (y - a_[a+1]) s'(h_[i+1]) a_i
+        # s'(h_[i+1]) = s(h_[i+1])(1 - s(h_[i+1])
+        # s(h_[i+1]) = a_[i+1]
+        for i in reversed(range(len(self.derivatives))):
+            activations = self.activations[i+1]
+            delta = error * self._sigmoid_derivative(activations)
+            # ndarray([0.1, 0.2]) --> ndarray([[0.1, 0.2]]
+            delta_reshaped = delta.reshape(delta.shape[0], -1).T
+            current_activations = self.activations[i]
+            # ndarray([0.1, 0.2]) --> ndarray([[0.1], [0.2]]
+            current_activations_reshaped = current_activations.reshape(current_activations.shape[0], -1)
+            self.derivatives[i] = np.dot(current_activations_reshaped, delta_reshaped)
+            error = np.dot(delta, self.weights[i].T)
+
+            if verbose:
+                print(f"Derivatives for W{i}: {self.derivatives[i]}")
+        return error
+
+
+    def _sigmoid_derivative(self, x):
+        return x * (1.0 - x)
 
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -35,14 +91,19 @@ class MLP:
 
 if __name__ == "__main__":
     # create an MLP
-    mlp = MLP()
+    mlp = MLP(2, [5], 1)
 
     # create some inputs
-    inputs = np.random.rand(mlp.num_inputs)
+    input = np.array([0.1, 0.2])
+    target = np.array([0.3])
 
     # perform forward prop
-    outputs = mlp.forward_propagate(inputs)
+    output = mlp.forward_propagate(input)
+
+    # calculate error
+    error = target - output
+
+    # back propagation
+    mlp.back_propagate(error, verbose=True)
 
     # print results
-    print(f"The network input is: {inputs}")
-    print(f"The network output is: {outputs}")

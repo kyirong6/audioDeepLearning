aed133c54814a031aea0fc55c6a95964b95173b5
ckyirong <ckyirong@sfu.ca>
2021-03-22 18:28:15 -0700

    added preprocessing for genre classification task

---
+++ 12-genre-classification/preprocess.py
@@ -0,0 +1,69 @@
+import os
+import librosa
+import math
+import json
+
+DATASET_PATH = "genre_dataset_reduced"
+JSON_PATH = "data.json"
+
+SAMPLE_RATE = 2250
+DURATION = 30 # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
+
+
+def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+    # dictionary to store data
+    data = {
+        "mapping": [],
+        "mfcc": [],
+        "labels": []
+    }
+
+    num_samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+    expected_num_mfcc_vectors_per_segment = math.ceil(num_samples_per_segment / hop_length)
+
+    # loop through all genres
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # ensure that we're not at the root level
+        if dirpath is not dataset_path:
+
+            # save the semantic label
+            dirpath_components = dirpath.split("/") # genre/blues => ["genre", "blues]
+            semantic_label = dirpath_components[-1]
+            data["mapping"].append(semantic_label)
+            print(f"\nProcessing {semantic_label}")
+
+            # process files for a specific genre
+            for f in filenames:
+                # load audio file
+                if f != ".DS_Store":
+                    file_path = os.path.join(dirpath, f)
+                    signal, sr = librosa.load(file_path, sr=SAMPLE_RATE)
+
+                    # process segments extracting mfcc and storing data
+                    for s in range(num_segments):
+                        start_sample = num_samples_per_segment * s
+                        finish_sample = start_sample + num_samples_per_segment
+
+                        mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample],
+                                                    sr=sr,
+                                                    n_fft=n_fft,
+                                                    n_mfcc=n_mfcc,
+                                                    hop_length=hop_length
+                                                    )
+                        mfcc = mfcc.T
+
+                        # store mfcc for segment if it has the expected length
+                        if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+                            data["mfcc"].append(mfcc.tolist()) # numpy array, must cast to python list to save in json
+                            data["labels"].append(i-1)
+                            print(f"{file_path}, segment:{s+1}")
+
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+
+
+if __name__ == "__main__":
+    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
+
